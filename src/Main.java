abd7f6654bc4fd8383cd82fd1350482f6890300f
Tokashiki Anzu <e245744@ie.u-ryukyu.ac.jp>
2024-12-16 00:42:58 +0900

    Step 3

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,11 @@
 public class Main{
     public static void main(String[] args) {
-        String str = null;
-        
-        try{
-            System.out.println(str.length());
-        }catch(NullPointerException e){
-            System.out.println("例外が発生しました : NullPointerException");
+        String str = "壱百満";
+
+        try {
+            int value = Integer.parseInt(str);
+        } catch (NumberFormatException e) {
+            System.out.println("例外が発生しました : NumberFormatException");
             System.out.println("例外のメッセージ : " + e.getMessage());
         }
     }
